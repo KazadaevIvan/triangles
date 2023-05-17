778773dff7ded92fa18191c0cb56098ba5945fa3
Ivan Kazadaew <i.ziliboba@mail.ru>
2023-05-16 23:12:24 -0700

    Add inputTriangleSides command

--- cypress/e2e/tests/triangles.cy.js
+++ cypress/e2e/tests/triangles.cy.js
@@ -7,9 +7,7 @@ describe('DDT Triangles app', () => {
 
   data.forEach(data => {
     it(data.name, () => {
-      cy.get('#side1').type(data.firstSide);
-      cy.get('#side2').type(data.secondSide);
-      cy.get('#side3').type(data.thirdSide);
+      cy.inputTriangleSides(data.firstSide, data.secondSide, data.thirdSide);
       cy.get('#identify-triangle-action').click();
       cy.get('#triangle-type').should('have.text', data.expectedMessage);
     });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('inputTriangleSides', (firstSide, secondSide, thirdSide, expectedMessage) => {
+    cy.get('#side1').type(firstSide);
+    cy.get('#side2').type(secondSide);
+    cy.get('#side3').type(thirdSide);
+  });
